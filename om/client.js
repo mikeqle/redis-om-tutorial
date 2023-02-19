db121c912fa22f3456fe82367c2f9d279df119d4
Mike Le <mike.le@live.com>
2023-02-19 15:34:06 +0700

    added API for search using params and full text search and location

--- om/client.js
+++ om/client.js
@@ -1,7 +1,14 @@
 import { Client } from "redis-om";
+import { createClient } from "redis";
 
 const url = process.env.REDIS_URL;
 
-const client = await new Client().open(url);
+export const connection = createClient(url);
+await connection.connect();
+
+// redis-om now using the redis client connection
+// we can use `connection` to access standard redis functionalities
+// while using `client` will give us access to the OMs
+const client = await new Client().use(connection);
 
 export default client;
